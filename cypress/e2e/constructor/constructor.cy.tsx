f0298b0774500f66054bb4bd1ec756fedd79da40
kkcaterina <sfonp@bk.ru>
2025-09-11 01:20:22 +0300

    added constructor cypress test

--- cypress/e2e/constructor/constructor.cy.tsx
+++ cypress/e2e/constructor/constructor.cy.tsx
@@ -3,3 +3,63 @@ describe('Проверяем доступность приложения', funct
     cy.visit('http://localhost:4000');
   });
 });
+
+describe('Проверяем работоспособность страницы конструктора бургера', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '**/api/ingredients', {
+      fixture: 'ingredients.json'
+    }).as('getIngredients');
+    cy.visit('/');
+    cy.wait('@getIngredients');
+    cy.get('[data-cy="643d69a5c3f7b9001cfa093d"]').as('purpleBun');
+    cy.get('[data-cy="643d69a5c3f7b9001cfa0949"]').as('saladMain');
+    cy.get('[data-cy="643d69a5c3f7b9001cfa094a"]').as('cheeseMain');
+    cy.get('[data-cy="643d69a5c3f7b9001cfa0946"]').as('ringsMain');
+    cy.get('[data-cy="643d69a5c3f7b9001cfa0943"]').as('spaceSauce');
+    cy.get('[data-cy="643d69a5c3f7b9001cfa0944"]').as('galacticSauce');
+  });
+  describe('Проверяем модальное окно ингредиента', () => {
+    it('Тестируем открытие модального окна ингредиента и сверяем данные ингредиента', () => {});
+    it('Тестируем закрытие модального окна ингредиента при клике на кнопку закрытия', () => {});
+    it('Тестируем закрытие модального окна ингредиента при клике на оверлей', () => {});
+  });
+  describe('Проверяем добавление ингредиента из списка ингредиентов в конструктор и создание заказа', () => {
+    it('Тестируем возможность добавления ингредиента в конструктор', () => {
+      cy.get('@purpleBun').contains('Добавить').click();
+      cy.get('@saladMain').contains('Добавить').click();
+      cy.get('@cheeseMain').contains('Добавить').click();
+      cy.get('@ringsMain').contains('Добавить').click();
+      cy.get('@spaceSauce').contains('Добавить').click();
+      cy.get('@galacticSauce').contains('Добавить').click();
+      cy.get('[data-cy="topItem"]').should(
+        'contain.text',
+        'Флюоресцентная булка R2-D3'
+      );
+      cy.get('[data-cy="burgerItems"]').should(
+        'contain.text',
+        'Мини-салат Экзо-Плантаго'
+      );
+      cy.get('[data-cy="burgerItems"]').should(
+        'contain.text',
+        'Сыр с астероидной плесенью'
+      );
+      cy.get('[data-cy="burgerItems"]').should(
+        'contain.text',
+        'Хрустящие минеральные кольца'
+      );
+      cy.get('[data-cy="burgerItems"]').should(
+        'contain.text',
+        'Соус фирменный Space Sauce'
+      );
+      cy.get('[data-cy="burgerItems"]').should(
+        'contain.text',
+        'Соус традиционный галактический'
+      );
+      cy.get('[data-cy="bottomItem"]').should(
+        'contain.text',
+        'Флюоресцентная булка R2-D3'
+      );
+    });
+    it('Тестируем оформление заказа', () => {});
+  });
+});
--- src/components/ui/burger-constructor/burger-constructor.tsx
+++ src/components/ui/burger-constructor/burger-constructor.tsx
@@ -20,7 +20,7 @@ export const BurgerConstructorUI: FC<BurgerConstructorUIProps> = ({
 }) => (
   <section className={styles.burger_constructor}>
     {constructorItems.bun ? (
-      <div className={`${styles.element} mb-4 mr-4`}>
+      <div className={`${styles.element} mb-4 mr-4`} data-cy='topItem'>
         <ConstructorElement
           type='top'
           isLocked
@@ -36,7 +36,7 @@ export const BurgerConstructorUI: FC<BurgerConstructorUIProps> = ({
         Выберите булки
       </div>
     )}
-    <ul className={styles.elements}>
+    <ul className={styles.elements} data-cy='burgerItems'>
       {constructorItems.ingredients.length > 0 ? (
         constructorItems.ingredients.map(
           (item: TConstructorIngredient, index: number) => (
@@ -57,7 +57,7 @@ export const BurgerConstructorUI: FC<BurgerConstructorUIProps> = ({
       )}
     </ul>
     {constructorItems.bun ? (
-      <div className={`${styles.element} mt-4 mr-4`}>
+      <div className={`${styles.element} mt-4 mr-4`} data-cy='bottomItem'>
         <ConstructorElement
           type='bottom'
           isLocked
